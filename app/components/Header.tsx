18383da2627b62ea1f881b3229d1ad873571a7c8
Tanish Singhal <tanishsinghal510@gmail.com>
2024-12-10 16:32:46 +0530

    user avatar feature added

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -10,12 +10,23 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [initialLoading, setInitialLoading] = useState(true);
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (status === "loading") return;
+
+    setInitialLoading(false);
+  }, [status]);
+
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container px-4 sm:px-6 lg:px-8 mx-auto">
+      <div className="px-4 sm:px-6 lg:px-8 mx-auto">
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center gap-8">
             <Link href="/" className="flex items-center">
@@ -49,9 +60,16 @@ const Header = () => {
           </div>
           <div className="flex items-center space-x-4">
             <ModeToggle />
-            <Button onClick={() => signIn("google")}>
-              Login
-            </Button>
+            {initialLoading && status === "loading" ? (
+              <div className="w-10 h-10 rounded-full bg-skeleton animate-pulse" />
+            ) : session ? (
+              <Avatar>
+                <AvatarImage src={session.user?.image || ""} />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            ) : (
+              <Button onClick={() => signIn("google")}>Login</Button>
+            )}
           </div>
         </div>
       </div>
