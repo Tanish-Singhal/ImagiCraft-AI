52f93ff62f428bd4411f2b5aa88ef51a2a9adff1
Tanish Singhal <tanishsinghal510@gmail.com>
2024-12-13 13:10:56 +0530

    prevent unauthorized user from image generation

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,7 +1,26 @@
+import { authOptions } from "@/app/utils/authOptions";
+import prisma from "@/app/utils/prisma";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized user" }, { status: 401 });
+  }
+
   const body = await request.json();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: session.user.id,
+    },
+  });
+
+  if (!user) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
 
   function randomeNumberGenerator() {
     return Math.floor(Math.random() * 1000000);
@@ -14,6 +33,15 @@ export async function POST(request: NextRequest) {
   )}?seed=${randomSeed}&width=512&height=512&nologo=True`;
 
   await fetch(imageURL);
+
+  await prisma.post.create({
+    data: {
+      prompt: body.imagePrompt,
+      url: imageURL,
+      seed: randomSeed,
+      userId: session.user.id,
+    },
+  });
 
   return NextResponse.json({
     url: imageURL,
--- app/utils/authOptions.ts
+++ app/utils/authOptions.ts
@@ -1,13 +1,30 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "./prisma";
+import { NextAuthOptions } from "next-auth";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    async session({session}) {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: session.user?.email || "",
+        },
+      });
+      
+      if (user) {
+        session.user = {
+          ...user
+        };
+      }
+      return session;
+    }
+  },
   adapter: PrismaAdapter(prisma),
 };
